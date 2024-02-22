f8e38b60551e8be86d59038d9570610d17d07b44
Osman Melih Tolunay <159262591+melihtolunay0@users.noreply.github.com>
2024-02-23 00:07:34 +0300

    change MemberCard

--- tearadio-front/app/page.tsx
+++ tearadio-front/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import MemberCard from "./member-card/page";
+import MemberCard from "./components/MemberCard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -9,7 +9,7 @@ export default function Home() {
         title="Digital Member Passport" 
         subtitle="MEMBERSHIP" 
         imageUrl="/profile.jpg"
-        
+
       />
     </main>
   );
