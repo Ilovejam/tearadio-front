f56b30c44f45a452b8387454b6db343b2223c558
Osman Melih Tolunay <159262591+melihtolunay0@users.noreply.github.com>
2024-02-23 15:31:45 +0300

    add chatbot

--- tearadio-front/app/page.tsx
+++ tearadio-front/app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+import React, { useEffect } from 'react';
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import MemberCard from "./components/MemberCard";
+declare global {
+  interface Window { Chatbot: any; }
+}
+
 export default function Home() {
+  useEffect(() => {
+    // Script etiketini oluştur
+        // Script etiketini oluştur
+        const script = document.createElement('script');
+        script.src = "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js";
+        script.type = 'module';
+        script.async = true;
+    
+        script.onload = () => {
+          // Script yüklendikten sonra Chatbot'u başlat
+          window.Chatbot.init({
+            chatflowid: "f52b5b25-2cf0-4e02-9134-c557de28450b",
+            apiHost: "https://business-chat.onrender.com",
+          });
+        };
+    
+    
+
+    // Script etiketini document'e ekle
+    document.body.appendChild(script);
+
+    // Unmount edildiğinde script'i kaldır
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
@@ -9,7 +42,6 @@ export default function Home() {
         title="Digital Member Passport" 
         subtitle="MEMBERSHIP" 
         imageUrl="/profile.jpg"
-
       />
     </main>
   );
