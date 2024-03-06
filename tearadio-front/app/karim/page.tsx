92b010f3fec0cf84290cc390cf32859fb534de40
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2024-03-06 08:32:03 +0300

    fix gix

--- tearadio-front/app/karim/page.tsx
+++ tearadio-front/app/karim/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import React from 'react'
 import IdCardStaff from '../components/IdCardStaff';
 import Navbar from '../components/Navbar';
+import { BubbleChat } from 'flowise-embed-react';
+
 const page = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -13,7 +16,7 @@ const page = () => {
           memberName="Karim Shehata"
           memberSinceDate="Jan 1, 2024"
         />
-
+    <BubbleChat chatflowid="df8d6be2-ec68-45a4-a7e4-9d833a492c79" apiHost="https://business-chat.onrender.com" />
 
   </div>
   )
