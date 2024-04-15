a4ae80eac0c1294843a353c005ddc31f26edacc6
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2024-04-15 06:46:46 +0300

    asd

--- tearadio-backend/server.js
+++ tearadio-backend/server.js
@@ -1,18 +1,12 @@
 const bodyParser = require('body-parser');
-const { Vonage } = require('@vonage/server-sdk');
 const cors = require('cors');
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;  // Eğer PORT çevre değişkeni tanımlıysa onu kullan, değilse 3000 portunu kullan
-const nodemailer = require('nodemailer');
 const mailjetTransport = require('nodemailer-mailjet-transport');
 const mailjet = require('node-mailjet').connect('7bbeb04b3e4eaf4beb93a606721c71d4', 'f371e106f4ff533448ddb950742de0ea');
 
-const vonage = new Vonage({
-  apiKey: process.env.VONAGE_API_KEY,
-  apiSecret: process.env.VONAGE_API_SECRET
-});
 
 
 
@@ -22,6 +16,9 @@ const transporter = nodemailer.createTransport(mailjetTransport({
     apiSecret: 'f371e106f4ff533448ddb950742de0ea' // Gizli Anahtarınız
   }
 }));
+const post = req.body;
+  posts.push(post);
+  res.status(201).send(post);
 
 
 app.use(cors());
@@ -29,37 +26,8 @@ app.use(bodyParser.json());
 let posts = []; // Bu array postlarımızı saklayacak.
 
 
-// SMS gönderme işlemi için Promise tabanlı bir fonksiyon
-function sendSms(from, to, text) {
-  return new Promise((resolve, reject) => {
-    vonage.sms.send({ from, to, text }, (err, responseData) => {
-      if (err) {
-        reject(err);
-      } else {
-        if (responseData.messages[0].status === "0") {
-          resolve(responseData);
-        } else {
-          reject(new Error(`Message failed with error: ${responseData.messages[0]['error-text']}`));
-        }
-      }
-    });
-  });
-}
 
-app.post('/send-sms', async (req, res) => {
-  const { phone, name, notes } = req.body;
-  const from = "Vonage APIs";
-  const text = `Hello ${name}, ${notes}`;
 
-  try {
-    await sendSms(from, phone, text); // SMS gönderme işlemini bekler
-    console.log("Message sent successfully.");
-    res.send({ success: true, message: 'Message sent successfully.' });
-  } catch (error) {
-    console.error('SMS gönderimi sırasında hata oluştu:', error);
-    res.status(500).send({ success: false, message: 'Failed to send SMS', error: error.message });
-  }
-});
 
 
 // Postları getirme endpoint'i
