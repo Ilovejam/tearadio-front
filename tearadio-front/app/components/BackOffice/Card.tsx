2e3f5fc6f9b8d2cdf764055f534321a58786c09f
melihtolunay0 <melih@letspepapp.com>
2024-04-15 07:26:14 +0300

    dsadasdas

--- tearadio-front/app/components/BackOffice/Card.tsx
+++ tearadio-front/app/components/BackOffice/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Card = ({ imageUrl, title, subtitle }) => (
-    <div className=" rounded-lg shadow overflow-hidden">
+const Card = ({ imageUrl, title, subtitle }: { imageUrl: string, title: string, subtitle: string }) => (
+  <div className=" rounded-lg shadow overflow-hidden">
       <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
       <div className="p-4">
         <h5 className="text-lg font-semibold">{title}</h5>
