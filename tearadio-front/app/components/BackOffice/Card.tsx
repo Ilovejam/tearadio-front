8c202930863a712381a4b45591449c07a558f987
melihtolunay0 <melih@letspepapp.com>
2024-04-15 07:44:38 +0300

    asd

--- tearadio-front/app/components/BackOffice/Card.tsx
+++ tearadio-front/app/components/BackOffice/Card.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Image from 'next/image';
 
 const Card = ({ imageUrl, title, subtitle }: { imageUrl: string, title: string, subtitle: string }) => (
   <div className=" rounded-lg shadow overflow-hidden">
-      <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
+<Image src={imageUrl} alt={title} height={48} />
       <div className="p-4">
         <h5 className="text-lg font-semibold">{title}</h5>
         <p className="text-sm text-gray-600">{subtitle}</p>
