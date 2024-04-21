5f61f2e34bb8f247be2e1c488267497a8ded26e6
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2024-04-21 19:54:12 +0300

    Update page.tsx

--- tearadio-front/app/notifications/page.tsx
+++ tearadio-front/app/notifications/page.tsx
@@ -4,8 +4,15 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import axios from 'axios';
 
+type Post = {
+  id: string;
+  author: string;
+  avatar: string;
+  content: string;
+};
+
 const Notifications = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   const apiUrl = 'https://bulldog-backend.vercel.app/posts';
 
   useEffect(() => {
@@ -14,32 +21,28 @@ const Notifications = () => {
 
   const fetchPosts = async () => {
     try {
-      const response = await axios.get(apiUrl);
+      const response = await axios.get<Post[]>(apiUrl);
       setPosts(response.data);
     } catch (error) {
       console.error("Error fetching posts:", error);
-      // Burada bir hata mesajı veya kullanıcıya bilgi verme işlemi yapabilirsiniz.
     }
   };
 
   const handleClear = async (id: string) => {
-  try {
-    await axios.delete(`${apiUrl}/${id}`);
-    fetchPosts();
-  } catch (error) {
-    console.error("Error deleting post:", error);
-    // Hata işleme kodunu buraya ekleyin
-  }
-};
-
+    try {
+      await axios.delete(`${apiUrl}/${id}`);
+      fetchPosts();
+    } catch (error) {
+      console.error("Error deleting post:", error);
+    }
+  };
 
   const handleClearAll = async () => {
     try {
-      await Promise.all(posts.map(post => axios.delete(`${apiUrl}/${post.id}`)));
-      fetchPosts();  // Re-fetch posts after all deletions
+      await Promise.all(posts.map((post) => axios.delete(`${apiUrl}/${post.id}`)));
+      fetchPosts();
     } catch (error) {
       console.error("Error clearing all posts:", error);
-      // Hata işleme kodunu buraya ekleyin
     }
   };
 
@@ -48,8 +51,8 @@ const Notifications = () => {
       <Navbar />
       <div className="flex flex-col items-center px-4 py-2 mt-20">
         {posts.length > 0 ? (
-          posts.map((post, index) => (
-            <div key={index} className="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-4 max-w-4xl w-full">
+          posts.map((post) => (
+            <div key={post.id} className="flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-4 max-w-4xl w-full">
               <div className="flex items-start px-4 py-6">
                 <img className="w-12 h-12 rounded-full object-cover mr-4 shadow" src={post.avatar} alt="avatar" />
                 <div className="flex-1">
